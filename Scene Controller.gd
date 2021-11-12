e2bb5ac2a8da23ae71ab824c40d8839e0ad91bf5
Nic <nicbarkdull@gmail.com>
2021-11-12 15:54:23 +0800

    First itch build uploaded to: https://whitelocke.itch.io/elementaball-wip

--- Scene Controller.gd
+++ Scene Controller.gd
@@ -24,16 +24,6 @@ var allPlayers = []
 
 func _ready():
 	set_process(true)
-	playerCenter = get_node("../Player")
-	playerLeft = get_node("../Player2")
-	playerRight = get_node("../Player3")
-	enemyCenter = get_node("../Enemy")
-	enemyLeft = get_node("../Enemy3")
-	enemyRight = get_node("../Enemy2")
-	
-	playerTeam = get_tree().get_nodes_in_group("player_team")
-	enemyTeam = [enemyCenter, enemyLeft, enemyRight]
-	allPlayers = [playerCenter, playerLeft, playerRight, enemyCenter, enemyLeft, enemyRight]
 
 func _process(delta):
 	if Input.is_action_pressed("ui_exit"):
