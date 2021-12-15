4e7bf37f7e1c2f159e073823c7e0a76a21fb4e50
Nic <nicbarkdull@gmail.com>
2021-12-15 11:20:01 +0800

    Added final score to match end menu

---
+++ UI/Player Select Menu/FinalScore.gd
@@ -0,0 +1,15 @@
+extends Label
+
+var blueScore = 0
+var redScore = 0
+
+func _ready():
+	SceneController.connect("goalScored", self, "score_goal")
+	
+func score_goal(kicker):
+	if kicker in get_tree().get_nodes_in_group("player_team"):
+		blueScore += 1
+	else:
+		redScore += 1
+		
+	self.text = "Final Score: Blue: " + str(blueScore) + " Red: " + str(redScore)
