e2d3dab99a968e842d521031d90ab1ccde4bf51c
Nic <nicbarkdull@gmail.com>
2021-12-23 10:53:59 +0800

    Players have preloaded abilities

--- Players/AbilityHandler.gd
+++ Players/AbilityHandler.gd
@@ -4,6 +4,7 @@ var abilityMenu1
 var abilityMenu2
 var abilityMenu3
 var abilityMenu4
+var abilityToLoad
 
 var selectableAbilities
 
@@ -13,50 +14,57 @@ func _ready():
 
 func _on_AbilityMenu1_initializeMenu(menu):
 	abilityMenu1 = menu
-	populate_menu(abilityMenu1)
+	populate_menu(abilityMenu1, abilityToLoad)
 	abilityMenu1.connect("item_selected",self,"ability1_item_selected")
 
-func populate_menu(menu):
+func populate_menu(menu, abilityToLoad):
 	menu.clear()
-	for ability in selectableAbilities:
-		menu.add_item(ability.name)
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	var selectedIndex = rng.randf_range(0, selectableAbilities.size()-1)
-	menu.select(selectedIndex)
+	if menu == abilityMenu1:
+		abilityToLoad = get_parent().ability1
+	elif menu == abilityMenu2:
+		abilityToLoad = get_parent().ability2
+	elif menu == abilityMenu3:
+		abilityToLoad = get_parent().ability3
+	elif menu == abilityMenu4:
+		abilityToLoad = get_parent().ability4
+	for ability in range(selectableAbilities.size()):
+		menu.add_item(selectableAbilities[ability].name)
+		if abilityToLoad:
+			if abilityToLoad.name == selectableAbilities[ability].name: 
+				menu.select(ability)
 
 func populate_all_menus():
 	var allMenus = [abilityMenu1, abilityMenu2, abilityMenu3, abilityMenu4]
 	for menu in allMenus:
 		if menu:
-			populate_menu(menu)
+			populate_menu(menu, null)
 
 func ability1_item_selected(index):
-	get_parent().ability1 = selectableAbilities[index-1]
+	get_parent().ability1 = selectableAbilities[index]
 	
 func ability2_item_selected(index):
-	get_parent().ability2 = selectableAbilities[index-1]
+	get_parent().ability2 = selectableAbilities[index]
 
 func ability3_item_selected(index):
-	get_parent().ability3 = selectableAbilities[index-1]
+	get_parent().ability3 = selectableAbilities[index]
 	
 func ability4_item_selected(index):
-	get_parent().ability4 = selectableAbilities[index-1]
+	get_parent().ability4 = selectableAbilities[index]
 
 
 func _on_AbilityMenu2_initializeMenu(menu):
 	abilityMenu2 = menu
-	populate_menu(abilityMenu2)
+	populate_menu(abilityMenu2, abilityToLoad)
 	abilityMenu2.connect("item_selected",self,"ability2_item_selected")
 
 
 func _on_AbilityMenu3_initializeMenu(menu):
 	abilityMenu3 = menu
-	populate_menu(abilityMenu3)
+	populate_menu(abilityMenu3, abilityToLoad)
 	abilityMenu3.connect("item_selected",self,"ability3_item_selected")
 
 
 func _on_AbilityMenu4_initializeMenu(menu):
 	abilityMenu4 = menu
-	populate_menu(abilityMenu4)
+	populate_menu(abilityMenu4, abilityToLoad)
 	abilityMenu4.connect("item_selected",self,"ability4_item_selected")
