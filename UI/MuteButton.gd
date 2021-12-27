d2bd8fbe30057c8da6829fc6bcdabc852e2b1aaa
Nic <nicbarkdull@gmail.com>
2021-12-27 13:44:05 +0800

    Mute icon changes according to mute toggle

--- UI/MuteButton.gd
+++ UI/MuteButton.gd
@@ -1,6 +1,9 @@
 extends ToolButton
 
 var muted = false
+export var mutedIcon : Texture
+export var unmutedIcon : Texture
+
 
 func _process(delta):
 	if Input.is_action_pressed("ui_mute"):
@@ -10,9 +13,9 @@ func _on_MuteButton_pressed():
 	self.muted = !self.muted
 	
 	if (self.muted):
-		print("SOUND MUTED")
+		self.icon = mutedIcon
 	else:
-		print("SOUND ENABLED")
+		self.icon = unmutedIcon
 	
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), self.muted)
 
