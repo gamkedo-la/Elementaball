fc8b6e3144022a6202fb9cf861a9313a866238f0
Nic <nicbarkdull@gmail.com>
2021-12-30 13:34:41 +0800

    Added sounds to menus

--- Sounds/AudioQueen.gd
+++ Sounds/AudioQueen.gd
@@ -4,6 +4,11 @@ extends Node2D
 signal playSound(sound)
 
 func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+	# when _ready is called, there might already be nodes in the tree, so connect all existing buttons
+	connect_buttons(get_tree().root)
+	get_tree().connect("node_added", self, "_on_SceneTree_node_added")
+	
 # warning-ignore:return_value_discarded
 	connect("playSound", self, "play_sound")
 	play_sound("Gameplay Music")
@@ -11,3 +16,31 @@ func _ready():
 func play_sound(sound):
 	if has_node(sound):
 		get_node(sound).play()
+
+func _on_SceneTree_node_added(node):
+	if node is Button:
+		connect_to_button(node)
+		node.connect("mouse_entered", self, "_on_Button_focus_entered")
+		node.connect("focus_entered", self, "_on_Button_focus_entered")
+	if node is OptionButton:
+		node.connect("item_focused", self, "_on_Button_focus_entered")
+		node.connect("item_selected", self, "_on_Button_pressed")
+	if node is PopupMenu:
+		node.connect("id_focused", self, "_on_Button_focus_entered")
+		node.connect("id_pressed", self, "_on_Button_pressed")		
+
+func _on_Button_pressed(_index = 0):
+	play_sound("Menu Select")
+	
+func _on_Button_focus_entered(_index = 0):
+	play_sound("Menu Up-Down")
+
+# recursively connect all buttons
+func connect_buttons(root):
+	for child in root.get_children():
+		if child is BaseButton:
+			connect_to_button(child)
+		connect_buttons(child)
+
+func connect_to_button(button):
+	button.connect("pressed", self, "_on_Button_pressed")
