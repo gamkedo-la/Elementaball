c058231ff7b9ea1249069db994dbdb9776e5b7ff
Nic <nicbarkdull@gmail.com>
2021-12-30 10:03:03 +0800

    Added quit button to Game Over menu

--- UI/Player Select Menu/EndGameMenuPopup.gd
+++ UI/Player Select Menu/EndGameMenuPopup.gd
@@ -6,8 +6,8 @@ export var restartButton : NodePath
 func _ready():
 	# warning-ignore:return_value_discarded
 	SceneController.connect("allKO",self,"_on_Timer_gameOver")
-	get_node(restartButton).grab_focus()
 
 func _on_Timer_gameOver(_winOrLose):
 	get_tree().paused = true
 	self.show()
+	get_node(restartButton).grab_focus()
